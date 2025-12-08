4a54b4556a809599172971a0f75602449d12f3fa
tresespadas <alvaro8038c@gmail.com>
2025-12-08 23:43:10 +0100

    falta algo

--- script_cert_digital.sh
+++ script_cert_digital.sh
@@ -179,21 +179,14 @@ ssl_apache2() {
   read -p "Nombre del directorio del sitio web del servidor: " DIR_SRV
   read -p "Nombre del certificado de la solicitud (ej. CertificadoServidor): " NOM_CERT_SOL
   read -p "Nombre de las claves (ej. ClavesCertificadoServidor): " NOM_CLAVE
-  #  read -r -s '' BLOQUE <<EOM
-  #    ServerSignature On
-  #    SSLEngine On
-  #    SSLCertificateFile /etc/ssl/${DIR_SRV}/${NOM_CERT_SOL}.pem
-  #    SSLCertificateKeyFile /etc/ssl/${DIR_SRV}/${NOM_CLAVE}.pem
-  #EOM
 
+  # Falta por mejorar las indentaciones
   sed -i "/<\/VirtualHost>/i \
-  ServerSignature On\n\
-  SSLEngine On\n\
-  SSLCertificateFile /etc/ssl/${DIR_SRV}/${NOM_CERT_SOL}.pem\n\
-  SSLCertificateKeyFile /etc/ssl/${DIR_SRV}/${NOM_CLAVE}.pem\n\
+      ServerSignature On\n\
+      SSLEngine On\n\
+      SSLCertificateFile /etc/ssl/${DIR_SRV}/${NOM_CERT_SOL}.pem\n\
+      SSLCertificateKeyFile /etc/ssl/${DIR_SRV}/${NOM_CLAVE}.pem\n\
   " "/etc/apache2/sites-available/${NOM_WEB_CONF}"
-
-  sed -i "/<\/VirtualHost>/i $BLOQUE" "/etc/apache2/sites-available/${NOM_WEB_CONF}"
 
   sed -i 's/<VirtualHost \*: *[0-9]\+>/<VirtualHost *:443>/g' /etc/apache2/sites-available/${NOM_WEB_CONF}
 
